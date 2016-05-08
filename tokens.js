dfbd501b57198b80dcc7d04b8d834c18018b2e1d
Albert Johnston <ajohn104@lion.lmu.edu>
2016-05-07 18:44:18 -0700

    Added tokens

---
+++ tokens.js
@@ -0,0 +1,32 @@
+var Tokens = {
+  Indent: 
+    ["  "],
+  Separator: 
+    [".", "{", "}", "[", "]", "(", ")", ":", ","],
+  OneCharacterOperators: 
+    ["+", "-", "*", "/", "^", "="],
+  WordOperators:
+    ["not", "and", "or", "equal_to", "not_equal_to", "greater_than", 
+     "less_than", "less_than_or_equal_to", "greater_than_or_equal_to"],
+  Reserved:
+    [ "declare", "as", "provide", "function", "needs", "type", 
+      "if", "else", "while", "foreach", "in", "of", "new", "with", "this"]
+  Comment: 
+    [">>"],
+  BoolLit: 
+    ["true", "false"],
+  Unused:
+    ["break", "class", "const", "continue", "debugger", "delete", "export", "extends",
+     "function", "import", "instanceof", "super", "typeof", "var", "void", "with",
+      "return", "try", "catch", "throw", "finally", "let", "do", "switch", "case", "defaults"]
+};
+
+// ErrUC = Error: Unexpected Characters, ErrUR = Error: Unused Reserved
+Tokens.kinds = 
+  [{ kind:"Newline", abbr: null}, {kind:"UnexpectedChars", abbr:"ErrUC"}, {kind:"Indent", abbr:null},
+   {kind:"Id", abbr: "Id"}, {kind:"Reserved", abbr: null}, {kind:"Unused", abbr: "ErrUR"},
+   {kind:"Comment", abbr: undefined}, {kind:"Separator", abbr: null}, {kind:"Operator", abbr: null},
+   {kind:"Native", abbr: null}, {kind:"BoolLit", abbr: "Bool"}, {kind:"IntLit", abbr: "Int"},
+   {kind:"StrLit", abbr: "Str"}, {kind:"Dedent", abbr: null, display: "\\d"} ];
+
+module.exports = Tokens;
